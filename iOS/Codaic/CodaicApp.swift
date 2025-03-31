e2bea480f6fc548bea82d5a38dfa30aaef68125b
Oliver Herklotz <oliver@hrkltz.io>
2025-03-31 22:23:55 +0200

    [*] Add iOS project.

---
+++ iOS/Codaic/CodaicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodaicApp.swift
+//  Codaic
+//
+//  Created by Oliver Herklotz on 31.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CodaicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
